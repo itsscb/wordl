6f61bda344ea16936a5eab35724437e6bfff1c25
itsscb <dev@itsscb.de>
2024-08-25 21:02:10 +0200

    fix: removes 'x-forwarded-for' and adds other client ip headers for reference

--- src/main.rs
+++ src/main.rs
@@ -34,17 +34,29 @@ async fn word() -> String {
 }
 
 async fn log_ip(req: Request<Body>, next: Next) -> Response {
-    let mut head = "x-forwarded-for";
+    let mut head = "REMOTE_ADDR";
     let ip = req
         .headers()
         .get(head)
         .and_then(|hv| hv.to_str().ok())
+        .or_else(|| {
+            head = "HTTP_CLIENT_IP";
+            req.headers().get(head).and_then(|hv| hv.to_str().ok())
+        })
         .or_else(|| {
             head = "x-real-ip";
             req.headers().get(head).and_then(|hv| hv.to_str().ok())
         })
         .or_else(|| {
             head = "cf-connection-ip";
+            req.headers().get(head).and_then(|hv| hv.to_str().ok())
+        })
+        .or_else(|| {
+            head = "HTTP_X_FORWARDED_FOR";
+            req.headers().get(head).and_then(|hv| hv.to_str().ok())
+        })
+        .or_else(|| {
+            head = "HTTP_FORWARDED_FOR";
             req.headers().get(head).and_then(|hv| hv.to_str().ok())
         })
         .unwrap_or("Unknown");
